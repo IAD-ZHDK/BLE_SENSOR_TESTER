4f514ba9364fbfb28ef750f0c49231643fb475d9
Luke Franzke <lukefranzke@gmail.com>
2020-09-15 18:07:52 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,110 @@
+import './css/style.css'
+import P5 from 'p5'
+import Ble from './js/Ble'
+// import * as dat from 'dat.gui'
+// todo: webpack is building with html file paths defaulting to root. This should be local to make it easer to host demos with github pages
+const containerElement = document.getElementById('p5-container')
+
+const sketch = (p) => {
+  let x = 100
+  let y = 100
+  let myFont
+  let myBLE
+  let chanelNames = ['Battery', 'Reference', 'Ch 6', 'Ch 5', 'Ch 4', 'Ch 3', 'Ch 2', 'Ch 1']
+  let histogram = new Array(8)
+  // Loop to create 2D array
+  for (let i = 0; i < histogram.length; i++) {
+    histogram[i] = new Array(1)
+  }
+
+  const serviceUuid = 'ff9c1e42-7b32-11ea-bc55-0242ac130003'
+  let sensorCharacteristic
+  let isConnected = false
+
+  // settings gui
+  const dat = require('dat.gui')
+  const gui = new dat.GUI()
+  let object1 = {
+    type3_number: 0
+  }
+  gui.remember(object1)
+
+  p.preload = function () {
+    myFont = p.loadFont('static/fonts/inconsolata.otf')
+  }
+
+  p.setup = function () {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+    myBLE = new Ble()
+    p.textFont(myFont)
+    p.textSize(p.width / 100)
+    p.fill(255)
+    p.noStroke()
+    p.textAlign(p.CENTER, p.CENTER)
+    let isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
+    if (!isChrome) {
+      window.alert('BLE may not work in your browser. Use Chrome or check for a list of compatible browsers here: https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API')
+    }
+    // setup settings gui
+    let filter = this.gui.addFolder('filter')
+    filter.add(object1, 'type3_number', -5, 5)
+    filter.open()
+  }
+
+  p.draw = function () {
+    p.clear()
+    if (myBLE.isConnected) {
+      // one sensor
+      // p.push()
+      // p.translate(p.windowWidth / 2, p.windowHeight / 2)
+      // let radius = p.map(myBLE.sensorValues[myBLE.sensorValues.length - 1], 0, 16384, 10, 500)
+      // p.ellipse(0, 0, radius, radius)
+      // p.text(myBLE.sensorValues[myBLE.sensorValues.length - 1], 0, p.windowHeight / 4)
+      // p.pop()
+
+      // p.textAlign(p.LEFT)
+      // p.text('battery: ' + myBLE.sensorValues[0], 20, 20)
+      // p.textAlign(p.CENTER, p.CENTER)
+      // remove first value (battery) and offset array
+      let sensorValues = []
+      for (let i = 0; i < myBLE.sensorValues.length - 1; i++) {
+        sensorValues.push(myBLE.sensorValues[i + 1])
+      }
+      let spacing = p.windowWidth / myBLE.sensorValues.length
+      p.translate((spacing / 2), p.windowHeight / 2)
+
+      for (let i = 0; i < myBLE.sensorValues.length; i++) {
+        p.push()
+        p.translate(spacing * i, 0)
+        let radius = p.map(myBLE.sensorValues[i], 0, 16384, 10, spacing * 0.3)
+        histogram[i].unshift(radius / 2)
+        p.stroke(100)
+        for (let j = 0; j < histogram[i].length; j++) {
+          p.line(-histogram[i][j], -j, histogram[i][j], -j)
+        }
+        if (histogram[i].length > 700) {
+          histogram[i].pop([histogram[i].length])
+        }
+        p.ellipse(0, 0, radius, radius)
+        p.noStroke()
+        p.translate(0, spacing / 3 * 4.0)
+        p.text(myBLE.sensorValues[i], 0, 0)
+        p.translate(0, p.textSize())
+        p.text(chanelNames[i], 0, 0)
+        p.pop()
+      }
+    } else {
+      p.translate(p.windowWidth / 2, p.windowHeight / 2)
+      p.text('No BLE Connection, click anywhere to pair BLE device', 0, 0)
+    }
+  }
+
+  p.touchStarted = function () {
+    myBLE.connectAndStartNotify()
+  }
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
+  }
+}
+
+let PFIVE = new P5(sketch, containerElement)
---
+++ src/js/Ble.js
@@ -0,0 +1,93 @@
+import P5 from 'p5'
+import P5ble from 'p5ble'
+let that
+let filtering = true
+class Ble {
+  constructor () {
+    console.log('looking for: A22A0001-AD0B-4DF2-A4E2-1745CBB4dCEE')
+    this.serviceUuid = 'A22A0001-AD0B-4DF2-A4E2-1745CBB4dCEE'
+    this.myBLE = new P5ble()
+    this.isConnected = false
+    this.sensorValues = []
+    this.sensorValues[0] = 0
+    this.sensorValues[1] = 0
+    this.sensorValues[2] = 0
+    this.sensorValues[3] = 0
+    this.sensorValues[4] = 0
+    this.sensorValues[5] = 0
+    this.sensorValues[6] = 0
+    this.sensorValues[7] = 0
+    that = this
+  }
+
+  connectAndStartNotify () {
+    // Connect to a device by passing the service UUID
+    this.myBLE.connect(this.serviceUuid, this.gotCharacteristics)
+  }
+  gotCharacteristics (error, characteristics) {
+    // A function that will be called once got characteristics
+    if (error) {
+      console.log('error: ', error)
+    } else {
+      console.log(characteristics[0])
+      // Check if myBLE is connected
+      console.log('check connection.')
+      that.isConnected = that.myBLE.isConnected()
+
+      // Add a event handler when the device is disconnected
+      that.myBLE.onDisconnected(that.onDisconnected)
+
+      for (let i = 0; i < characteristics.length; i++) {
+        if (i === 0) {
+          const sensorCharacteristic = characteristics[i]
+          // Set datatype to 'custom', p5.ble.js won't parse the data, will return data as it is.
+          that.myBLE.startNotifications(sensorCharacteristic, that.handleSensor, 'custom')
+          console.log('characteristics: 1')
+        } else if (i === 1) {
+          console.log('characteristics: 2')
+        } else if (i === 2) {
+          console.log('characteristics: 3')
+        } else {
+          console.log("characteristic doesn't match.")
+        }
+      }
+    }
+  }
+
+  onDisconnected () {
+    console.log('Device was disconnected.')
+    this.isConnected = false
+  }
+
+  handleSensor (data) {
+    if (filtering) {
+      let factor = 0.01
+      that.sensorValues[0] = Math.floor(that.sensorValues[0] * factor)
+      that.sensorValues[1] = Math.floor(that.sensorValues[1] * factor)
+      that.sensorValues[2] = Math.floor(that.sensorValues[2] * factor)
+      that.sensorValues[3] = Math.floor(that.sensorValues[3] * factor)
+      that.sensorValues[4] = Math.floor(that.sensorValues[4] * factor)
+      that.sensorValues[5] = Math.floor(that.sensorValues[5] * factor)
+      that.sensorValues[6] = Math.floor(that.sensorValues[6] * factor)
+      that.sensorValues[7] = Math.floor(that.sensorValues[7] * factor)
+      that.sensorValues[0] += Math.floor(data.getUint16(0, true) * (1.0 - factor))
+      that.sensorValues[1] += Math.floor(data.getUint16(2, true) * (1.0 - factor))
+      that.sensorValues[2] += Math.floor(data.getUint16(4, true) * (1.0 - factor))
+      that.sensorValues[3] += Math.floor(data.getUint16(6, true) * (1.0 - factor))
+      that.sensorValues[4] += Math.floor(data.getUint16(8, true) * (1.0 - factor))
+      that.sensorValues[5] += Math.floor(data.getUint16(10, true) * (1.0 - factor))
+      that.sensorValues[6] += Math.floor(data.getUint16(11, true) * (1.0 - factor))
+      that.sensorValues[7] += Math.floor(data.getUint16(12, true) * (1.0 - factor))
+    } else {
+      that.sensorValues[0] = data.getUint16(0, true)
+      that.sensorValues[1] = data.getUint16(2, true)
+      that.sensorValues[2] = data.getUint16(4, true)
+      that.sensorValues[3] = data.getUint16(6, true)
+      that.sensorValues[4] = data.getUint16(8, true)
+      that.sensorValues[5] = data.getUint16(10, true)
+      that.sensorValues[6] = data.getUint16(11, true)
+      that.sensorValues[7] = data.getUint16(12, true)
+    }
+  }
+}
+export default Ble
